07a9e74eed0a7d5f4dacb5fa07ff4e656455bc6e
e34rrsff <dev@e34.tech>
2024-10-31 00:18:50 -0700

    Added removal of unauthorized files
    My tty is broken rn from tryng to instal nvidia drivers. dunno if this will look right.

--- ubuntu.sh
+++ ubuntu.sh
@@ -69,3 +69,69 @@ Press any key to continue..."
 
 	fi
 done
+
+echo -e "\033[34m\
+###############################\n\
+# Removing Unauthorized Files #\n\
+###############################\
+\033[0m"
+
+echo -e "\033[33m\n\
+Check users' home directories for unauthorized files?\
+\033[0m\n"
+
+echo -en "(Y/N)?:"
+while :
+do
+	if [[ "$CHECK_UNAUTH_FILES" == 'Y' || "$CHECK_UNAUTH_FILES" == 'y' ]]; then
+		echo -e "\n\033[32m\
+Give the command several seconds to find the files; then review them.\n\
+Press Q when you are done reviewing the files.\n\
+\033[0m\n\
+Press any key to continue..."
+		read -n 1
+		sudo find /home \
+-iname "*.mp3" -o \
+-iname "*.mp4" -o \
+-iname "*.mov" -o \
+-iname "*.wmv" -o \
+-iname "*.avi" -o \
+-iname "*.gif" -o \
+-iname "*.webm" 2>/dev/null | less
+
+		while :
+		do
+			if [[ "$MV_FILES" == 'Y' || "$MV_FILES" == 'y' ]]; then
+				echo -e "Moving unauthorized files to /tmp/unauthorized-files/...'"
+				sudo mkdir -p /tmp/unauthorized-files/
+				sudo find /home \
+-iname "*.mp3" -o \
+-iname "*.mp4" -o \
+-iname "*.mov" -o \
+-iname "*.wmv" -o \
+-iname "*.avi" -o \
+-iname "*.gif" -o \
+-iname "*.webm" -exec cp --parents {} /tmp/unauthorized-files/ \; -exec rm {} \;
+				sudo chown -R root:root /tmp/unauthorized-files/
+				break
+			elif [[ "$MV_FILES" == 'N' || "$MV_FILES" == 'n' ]]; then
+				echo -e "Skipped removing unauthorized files...\n"
+				break
+			else
+				echo -e "\n\033[33m\
+Would you like to remove the files that were listed?\
+\033[0m"
+				echo -en "Enter \"Y\" or \"N\":"
+				read 'MV_FILES'
+			fi
+		done
+		break
+	elif [[ "$CHECK_UNAUTH_FILES" == 'N' || "$CHECK_UNAUTH_FILES" == 'n' ]]; then
+		echo -e "Skipping check...\n"
+		break
+	else
+		echo -en "Enter \"Y\" or \"N\":"
+		read 'CHECK_UNAUTH_FILES'
+
+	fi
+done
