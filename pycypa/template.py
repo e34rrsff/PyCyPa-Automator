084d892246e4539cb230b7d64725c87a4590d77b
e34rrsff <dev@e34.tech>
2024-11-17 00:29:02 -0800

    I dunno how i deleted everything
    Hopefully it's all back

---
+++ pycypa/template.py
@@ -0,0 +1,9 @@
+# Mozilla General Public License Version 2.0
+# Copyright 2024 PyCyPa Automator Developers.
+# See the COPYRIGHT file at the top-level directory.
+
+# main() doesn't need to be defined for scripts that only contain functions,
+# such as scripts under .../<distro-codename/windows-release>/
+
+def main():
+    # Write your code here
