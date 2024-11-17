f291bc48fde11ca733fb7356817eca476eb12017
e34rrsff <dev@e34.tech>
2024-11-17 00:27:23 -0800

    Made custom license

--- pycypa-automator/template.py
+++
@@ -1,9 +0,0 @@
-# Mozilla General Public License Version 2.0
-# Copyright 2024 PyCyPa Automator Developers.
-# See the COPYRIGHT file at the top-level directory.
-
-def main():
-    # Write your code here
-
-if __name__ == '__main__':
-    main()
