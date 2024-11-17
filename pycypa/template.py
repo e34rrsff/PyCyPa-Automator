85304ac376a208d95e19301de637f3b26c6e6c1f
e34rrsff <dev@e34.tech>
2024-11-17 01:56:53 -0800

    Forgor updating template copyright notice

--- pycypa/template.py
+++ pycypa/template.py
@@ -1,5 +1,7 @@
 # Copyright 2024 PyCyPa Automator Developers.
-# See the COPYRIGHT file at the top-level directory.
+# This file is subject to copyright restrictions defined by
+# the COPYRIGHT file located at the top-level-directory of
+# this codebase.
 
 # main() doesn't need to be defined for scripts that only contain functions,
 # such as scripts under .../<distro-codename/windows-release>/
